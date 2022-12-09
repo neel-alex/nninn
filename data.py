69750b04f17ab70890181ca782b94fa245fa34f9
Neel Alex <alexneel@gmail.com>
2022-12-09 15:48:45 +0000

    Sacred cleanup.

--- data.py
+++ data.py
@@ -2,19 +2,53 @@ from jax import random
 import jax.numpy as jnp
 from optax import adam, sgd
 import haiku as hk
+from sacred import Experiment, observers
 
 from mnist import mnist
-from model import net_fn, train_net, train_nets
+from model import net_fn, train_nets
+
+train_nets_experiment = Experiment('train_nets')
+observer = observers.FileStorageObserver('results/train_nets')
+train_nets_experiment.observers.append(observer)
 
 
-def gather_network_dataset(key, network, optimiser, train_data, train_labels, test_data, test_labels, n_nets=100):
+@train_nets_experiment.config
+def config():
+    n_classes = 10
+    n_hidden = 16
+    batch_size = 60000  # 60000 is full-batch
+    learning_rate = 1e-3
+    n_nets = 100  # Number per setting -- 2x this many networks will be created
+    n_parallel = 100  # How many nets to train in parallel
+    seed = 4  # Chosen by fair dice roll. Guaranteed to be random.
+
+
+@train_nets_experiment.capture
+def gather_network_dataset(key,
+                           network,
+                           optimiser,
+                           train_data,
+                           train_labels,
+                           test_data,
+                           test_labels,
+                           n_nets,
+                           n_parallel):
     nets = []
-    for i in range(N_RUNS):
+    assert (n_nets / n_parallel) % 1 == 0, "Please ensure that the parallelization evenly divides the number of nets."
+    n_runs = n_nets // n_parallel
+    for i in range(n_runs):
         key, subkey = random.split(key)
-        net_params = train_nets(subkey, network, optimiser, train_data, train_labels, test_data, test_labels,
-                                n_nets=n_nets, verbose=1)
+        net_params = train_nets(subkey,
+                                network,
+                                optimiser,
+                                train_data,
+                                train_labels,
+                                test_data,
+                                test_labels,
+                                n_nets=n_parallel,
+                                verbose=1)
 
-        for j in range(n_nets):
+        for j in range(n_parallel):
             flat_net = jnp.array([])
             for layer in net_params:
                 for param in net_params[layer]:
@@ -24,36 +58,29 @@ def gather_network_dataset(key, network, optimiser, train_data, train_labels, te
     return nets
 
 
-NUM_CLASSES = 10
-NUM_HIDDEN = 16
-BATCH_SIZE = 60000
-LEARNING_RATE = 1e-3
-NUM_DATAPOINTS = 200
-N_PARALLEL = 100
-N_RUNS = (NUM_DATAPOINTS // N_PARALLEL) // 2
-SEED = 4
-
-key = random.PRNGKey(SEED)
-
-train_data, train_labels, test_data, test_labels = mnist()
-
-network = hk.without_apply_rng(hk.transform(lambda x: net_fn(x, NUM_HIDDEN, NUM_CLASSES)))
-
-optimiser = adam(LEARNING_RATE)
-key, subkey = random.split(key)
-networks = gather_network_dataset(subkey, network, optimiser, train_data, train_labels,
-                                  test_data, test_labels, n_nets=N_PARALLEL)
-optimiser = sgd(LEARNING_RATE)
-key, subkey = random.split(key)
-networks += gather_network_dataset(subkey, network, optimiser, train_data, train_labels,
-                                   test_data, test_labels, n_nets=N_PARALLEL)
-
-data_nets = jnp.row_stack(networks)
-data_labels = jnp.array([0 for _ in range(NUM_DATAPOINTS//2)] +
-                        [1 for _ in range(NUM_DATAPOINTS//2)])
-jnp.savez(f'./data/networks_{data_nets.shape[0]}.npz', nets=data_nets, labels=data_labels)
+@train_nets_experiment.capture
+def create_network(n_hidden, n_classes):
+    return hk.without_apply_rng(hk.transform(lambda x: net_fn(x, n_hidden, n_classes)))
 
 
+@train_nets_experiment.automain
+def main(learning_rate, n_nets, seed):
+    train_data, train_labels, test_data, test_labels = mnist()
+    network = create_network()
+    key = random.PRNGKey(seed)
 
+    optimiser = adam(learning_rate)
+    key, subkey = random.split(key)
+    networks = gather_network_dataset(subkey, network, optimiser, train_data, train_labels,
+                                      test_data, test_labels, n_nets=n_nets)
 
+    optimiser = sgd(learning_rate)
+    key, subkey = random.split(key)
+    networks += gather_network_dataset(subkey, network, optimiser, train_data, train_labels,
+                                       test_data, test_labels, n_nets=n_nets)
 
+    data_nets = jnp.row_stack(networks)
+    data_labels = jnp.array([0 for _ in range(n_nets)] +
+                            [1 for _ in range(n_nets)])
+    print(f"Saving {data_nets.shape[0]} networks.")
+    jnp.savez(f'./data/networks_{data_nets.shape[0]}.npz', nets=data_nets, labels=data_labels)
--- main.py
+++ main.py
@@ -2,40 +2,58 @@ from jax import random
 import jax.numpy as jnp
 from optax import adam
 import haiku as hk
+from sacred import Experiment, observers
 
 from model import net_fn, train_net
 
 
-NUM_CLASSES = 2
-NUM_HIDDEN = 256
-LEARNING_RATE = 1e-3
-SEED = 4
-NUM_DATAPOINTS = 200
-TRAIN_FRAC = 0.8
+interp_net_experiment = Experiment('interp_net')
+observer = observers.FileStorageObserver('results/interp_net')
+interp_net_experiment.observers.append(observer)
 
 
-key = random.PRNGKey(SEED)
+@interp_net_experiment.config
+def config():
+    n_classes = 2
+    n_hidden = 256
+    learning_rate = 1e-3
+    n_data = 2000
+    train_frac = 0.8
+    normalize = False
+    seed = 4  # Chosen by fair dice roll. Guaranteed to be random.
 
-network = hk.without_apply_rng(hk.transform(lambda x: net_fn(x, NUM_HIDDEN, NUM_CLASSES)))
-optimiser = adam(LEARNING_RATE)
 
-filestore = jnp.load(f'./data/networks_{NUM_DATAPOINTS}.npz')
-nets = jnp.array(filestore['nets'])
-labels = jnp.array(filestore['labels'])
+@interp_net_experiment.capture
+def create_network(n_hidden, n_classes):
+    return hk.without_apply_rng(hk.transform(lambda x: net_fn(x, n_hidden, n_classes)))
 
-key, subkey = random.split(key)
-n_datapoints = nets.shape[0]
-perm = random.permutation(subkey, n_datapoints)
-nets = nets[perm]
-labels = labels[perm]
-labels = jnp.eye(NUM_CLASSES)[labels]
 
-split = int(n_datapoints*TRAIN_FRAC)
+@interp_net_experiment.automain
+def main(learning_rate, n_classes, n_data, train_frac, normalize, seed):
+    network = create_network()
+    optimiser = adam(learning_rate)
 
-train_nets = nets[:split]
-test_nets = nets[split:]
-train_labels = labels[:split]
-test_labels = labels[split:]
+    filestore = jnp.load(f'./data/networks_{n_data}.npz')
+    nets = jnp.array(filestore['nets'])
+    labels = jnp.array(filestore['labels'])
 
-key, subkey = random.split(key)
-train_net(key, network, optimiser, train_nets, train_labels, test_nets, test_labels, verbose=2)
+    if normalize:
+        nets = nets / jnp.linalg.norm(nets, axis=1).reshape(-1, 1)
+
+    key = random.PRNGKey(seed)
+    key, subkey = random.split(key)
+
+    perm = random.permutation(subkey, n_data)
+    nets = nets[perm]
+    labels = labels[perm]
+    labels = jnp.eye(n_classes)[labels]
+
+    split = int(n_data*train_frac)
+
+    train_nets = nets[:split]
+    test_nets = nets[split:]
+    train_labels = labels[:split]
+    test_labels = labels[split:]
+
+    key, subkey = random.split(key)
+    train_net(key, network, optimiser, train_nets, train_labels, test_nets, test_labels, verbose=2)
